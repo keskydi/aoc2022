af71fb3a2d3cddc2cf7edb9b0beb3442036a7524
keskydi <antoine.orhant@gmail.com>
2022-12-09 21:00:49 +0100

    day 08

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,11 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     
-    let now = std::time::Instant::now();
-    let mut elf_calories = Vec::new();
-    let mut elf_total = 0;
+    let iter = vec![1,2,3,4,5,6,7,8,9,10];
 
-    // Open the file in read-only mode
-    let file = File::open("src/day1/input.in").unwrap();
+    dbg!(iter[0..0].iter().step_by(3).collect::<Vec<_>>());
+    dbg!(iter[0..8].iter().rev().step_by(3).collect::<Vec<_>>());
+    dbg!(iter[0..8].iter().step_by(3).rev().collect::<Vec<_>>());
+    dbg!(iter[0..8].iter().rev().collect::<Vec<_>>());
 
-    // Create a BufReader instance to read the file line by line
-    let reader = BufReader::new(file);
-
-    // Read the file, one line at a time
-    for line in reader.lines() {
-        let line = line.unwrap();
-        if line.is_empty() {
-            // An empty line signals the end of an Elf's inventory
-            elf_calories.push(elf_total);
-            elf_total = 0;
-        } else {
-            // Add the calories of the current food item to the Elf's total
-            elf_total += line.trim().parse::<i32>().unwrap();
-        }
-    }
-
-    // Find the Elf with the most calories and print the result
-    let max_calories = elf_calories.iter().max().unwrap();
-    println!("Elf with most calories: {}", max_calories);
-    
-    println!("time: {:?}", now.elapsed());
 }
