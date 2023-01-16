753e033ce168604152c7c1ab16d9cffd44e3eb60
keskydi <antoine.orhant@gmail.com>
2023-01-16 22:40:12 +0100

    optimisation jour16

--- src/day16/main.rs
+++ src/day16/main.rs
@@ -1,9 +1,4 @@
-// #![allow(warnings)]
-use std::{
-    cell::RefCell,
-    collections::{HashMap, VecDeque},
-    fs,
-};
+use std::{collections::HashMap, fs};
 
 use nom::{
     branch::alt,
@@ -15,169 +10,51 @@ use nom::{
     IResult, Parser,
 };
 
-type R = String;
+// return sum of pressure release
 
-fn level_1(input: &str) -> R {
-    unimplemented!()
-}
+fn floyd_warshall(graph: &[(&str, u8, Vec<&str>)]) -> Vec<Vec<u8>> {
+    let num_of_nodes = graph.len();
 
-fn level_2(input: &str) -> R {
-    unimplemented!()
-}
+    let valve_name_to_idx: HashMap<&str, _> = graph
+        .iter()
+        .enumerate()
+        .map(|(i, &(id, ..))| (id, i))
+        .collect();
 
-struct Graph<'a> {
-    points: HashMap<&'a str, Node<'a>>,
-    // points: HashMap<&'a str, (u32, Vec<&'a str>)>,
-}
+    // |V|x|V| matrix
+    let mut dist = vec![vec![u8::MAX; num_of_nodes]; num_of_nodes];
 
-#[derive(Debug)]
-struct Node<'a> {
-    flow_rate: u32,
-    edges: Vec<&'a str>,
-    dist_edges: RefCell<Vec<(&'a str, u32)>>,
-}
-
-impl<'a> Node<'a> {
-    fn new(flow_rate: u32, edges: Vec<&'a str>) -> Self {
-        Node {
-            flow_rate,
-            edges,
-            dist_edges: RefCell::new(Vec::new()),
+    for (i, (_, _, edges)) in graph.iter().enumerate() {
+        for edge in edges {
+            let y = valve_name_to_idx[edge];
+            dist[i][y] = 1;
         }
+        dist[i][i] = 0;
     }
-}
+    // init distances of paths with no intermediate nodes
 
-impl<'a> Graph<'a> {
-    // return sum of pressure release
-    fn minimax(&self) -> u32 {
-        // self.traverse_alone("AA", vec![], 31)
-        self.traverse_with_elephant("AA", "AA", vec![], 27, 27)
-    }
-
-    fn resolve_edges_distances(&mut self) {
-        // let keys = self
-        //     .points
-        //     .iter()
-        //     .filter_map(|(&k, v)| if v.flow_rate > 0 { Some(k) } else { None })
-        //     .collect::<Vec<&str>>();
-
-        for (key, node) in self.points.iter() {
-            let mut q: VecDeque<(&str, u32)> = node.edges.iter().map(|&x| (x, 1)).collect();
-            let mut vst: Vec<&str> = vec![key];
-
-            while let Some((id, dist)) = q.pop_front() {
-                vst.push(id);
-                let current_node = self.points.get(id).unwrap();
-                let mut q1: VecDeque<(&str, u32)> = current_node
-                    .edges
-                    .iter()
-                    .filter_map(|&x| {
-                        if !vst.contains(&x) {
-                            Some((x, dist + 1))
-                        } else {
-                            None
-                        }
-                    })
-                    .collect();
-                q.append(&mut q1);
-
-                if current_node.flow_rate > 0 {
-                    node.dist_edges.borrow_mut().push((id, dist));
+    for k in 0..num_of_nodes {
+        for i in 0..num_of_nodes {
+            for j in 0..num_of_nodes {
+                let (result, overflow) = dist[i][k].overflowing_add(dist[k][j]);
+                if !overflow && dist[i][j] > result {
+                    dist[i][j] = result;
                 }
             }
         }
     }
 
-    fn traverse_alone(&self, node: &'a str, mut vst: Vec<&'a str>, days_left: u32) -> u32 {
-        let current = self.points.get(node).unwrap();
-
-        vst.push(node);
-
-        let pressure_release = current.flow_rate * (days_left - 1);
-
-        current
-            .dist_edges
-            .borrow()
-            .iter()
-            .map(|(edge, dist)| {
-                if (dist + 1) < days_left && !vst.contains(edge) {
-                    self.traverse_alone(edge, vst.clone(), days_left - dist - 1)
-                } else {
-                    0_u32
-                }
-            })
-            .max()
-            .unwrap()
-            + pressure_release
-    }
-
-    fn traverse_with_elephant(
-        &self,
-        node1: &'a str,
-        node2: &'a str,
-        mut vst: Vec<&'a str>,
-        days_left1: u32,
-        days_left2: u32,
-    ) -> u32 {
-        let c1 = self.points.get(node1).unwrap();
-        let c2 = self.points.get(node2).unwrap();
-
-        vst.push(node1);
-        vst.push(node2);
-
-        let p1 = c1.flow_rate * (days_left1 - 1);
-        let p2 = c2.flow_rate * (days_left2 - 1);
-
-        c1.dist_edges
-            .borrow()
-            .iter()
-            .map(|(e1, d1)| {
-                if !vst.contains(e1){
-                c2.dist_edges
-                    .borrow()
-                    .iter()
-                    .map(|(e2, d2)| {
-                        if e1 != e2  && !vst.contains(e2) {
-                            match ((d1 + 1) < days_left1, (d2 + 1) < days_left2) {
-                                (true, true) => self.traverse_with_elephant(
-                                    e1,
-                                    e2,
-                                    vst.clone(),
-                                    0.max(days_left1 - d1 - 1),
-                                    0.max(days_left2 - d2 - 1),
-                                ),
-                                (true, false) => {
-                                    self.traverse_alone(e1, vst.clone(), days_left1 - d1 - 1)
-                                }
-                                (false, true) => {
-                                    self.traverse_alone(e2, vst.clone(), days_left2 - d2 - 1)
-                                }
-                                (false, false) => 0,
-                            }
-                        } else {
-                            0_u32
-                        }
-                    })
-                    .max()
-                    .unwrap()}else{
-                        0
-                    }
-            })
-            .max()
-            .unwrap()
-            + p1
-            + p2
-    }
+    dist
 }
 
-fn parse_input<'a>(s: &'a str) -> IResult<&'a str, Graph<'a>> {
+fn parse_input(s: &str) -> IResult<&str, Vec<(&str, u8, Vec<&str>)>> {
     separated_list1(
         line_ending,
         separated_pair(
             separated_pair(
                 preceded(tag::<_, _, Error<_>>("Valve "), alpha1),
                 tag(" has flow rate="),
-                complete::u32,
+                complete::u8,
             ),
             alt((
                 tag("; tunnels lead to valves "),
@@ -185,27 +62,149 @@ fn parse_input<'a>(s: &'a str) -> IResult<&'a str, Graph<'a>> {
             )),
             separated_list0(tag(", "), alpha1),
         )
-        .map(|((ident, rate), edges)| (ident, Node::new(rate, edges))),
+        .map(|((ident, rate), edges)| (ident, rate, edges)),
     )(s)
-    .map(|(s, points)| {
-        (
-            s,
-            Graph {
-                points: points.into_iter().collect::<HashMap<&str, Node<'_>>>(),
-            },
-        )
-    })
 }
 fn main() {
+    let data = fs::read_to_string("src/day16/input.in").expect("file not found");
+
+    // let (_, mut graph) = parse_input(&input).unwrap();
+    let (_, rows) = parse_input(&data).unwrap();
+    let shortest_path_lengths_uncompressed = floyd_warshall(&rows);
+
+    let interesting_values: Vec<usize> = rows
+        .iter()
+        .enumerate()
+        .filter_map(|(id, &(name, flow, _))| {
+            if name == "AA" || flow > 0 {
+                Some(id)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let flow_rate = interesting_values
+        .iter()
+        .map(|&i| rows[i].1)
+        .collect::<Vec<u8>>();
+
+    let shortest_path_lengths: Vec<Vec<u8>> = interesting_values
+        .iter()
+        .map(|&i| {
+            interesting_values
+                .iter()
+                .map(|&y| shortest_path_lengths_uncompressed[i][y])
+                .collect()
+        })
+        .collect();
+
+    let starting_node = interesting_values
+        .iter()
+        .position(|&i| rows[i].0 == "AA")
+        .unwrap();
+
     let now = std::time::Instant::now();
-    let input = fs::read_to_string("src/day16/input.in").expect("file not found");
 
-    let (_, mut graph) = parse_input(&input).unwrap();
+    println!(
+        "level_1: {}",
+        bfs_part1(&flow_rate, &shortest_path_lengths, starting_node, 0, 31)
+    );
 
-    graph.resolve_edges_distances();
+    println!(
+        "level_2: {}",
+        bfs_part2(
+            &flow_rate,
+            &shortest_path_lengths,
+            starting_node,
+            starting_node,
+            0,
+            27,
+            27
+        )
+    );
 
-    println!("level_1: {}", graph.minimax());
-    // println!("level_2: {}", level_2(&input));
+    println!("Time: {:?}", now.elapsed());
+}
 
-    println!("time: {:?}", now.elapsed());
+fn bfs_part1(
+    flow_rates: &Vec<u8>,
+    shortest_path_lengths: &Vec<Vec<u8>>,
+    indice: usize,
+    mut vst: usize,
+    time_left: u8,
+) -> usize {
+    vst |= 1 << indice;
+
+    let pressure_release = flow_rates[indice] as usize * (time_left - 1) as usize;
+
+    let result = shortest_path_lengths[indice]
+        .iter()
+        .enumerate()
+        .map(|(i, dist)| {
+            if dist + 1 < time_left && vst & 1 << i == 0 {
+                bfs_part1(
+                    flow_rates,
+                    shortest_path_lengths,
+                    i,
+                    vst,
+                    time_left - dist - 1,
+                )
+            } else {
+                0
+            }
+        })
+        .max()
+        .unwrap()
+        + pressure_release;
+
+    result
+}
+
+fn bfs_part2(
+    flow_rates: &Vec<u8>,
+    shortest_path_lengths: &Vec<Vec<u8>>,
+    indice1: usize,
+    indice2: usize,
+    mut vst: usize,
+    time_left1: u8,
+    time_left2: u8,
+) -> usize {
+    vst |= 1 << indice1 | 1 << indice2;
+
+    let pressure_release = flow_rates[indice1] as usize * (time_left1 - 1) as usize
+        + flow_rates[indice2] as usize * (time_left2 - 1) as usize;
+
+    shortest_path_lengths[indice1]
+        .iter()
+        .enumerate()
+        .map(|(i1, dist1)| {
+            if dist1 + 1 < time_left1 && vst & 1 << i1 == 0 {
+                shortest_path_lengths[indice2]
+                    .iter()
+                    .enumerate()
+                    .map(|(i2, dist2)| {
+                        if dist2 + 1 < time_left2 && vst & 1 << i2 == 0 && i1 != i2 {
+                            bfs_part2(
+                                flow_rates,
+                                shortest_path_lengths,
+                                i1,
+                                i2,
+                                vst,
+                                time_left1 - dist1 - 1,
+                                time_left2 - dist2 - 1,
+                            )
+                        } else {
+                            0
+                        }
+                    })
+                    .max()
+                    .unwrap()
+            } else {
+                0
+            }
+        })
+        .max()
+        .unwrap()
+        + pressure_release
 }
