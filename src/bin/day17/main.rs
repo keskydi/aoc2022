0c7729e82552e0d08e9eeee735b78ef03e80fcb2
keskydi <antoine.orhant@gmail.com>
2023-02-03 01:37:13 +0100

    day 17 & 18

--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -1,23 +1,211 @@
-use std::fs;
+use std::{collections::HashMap, fmt, fs};
 
-type R = String;
-
-fn level_1(input: &str) -> R {
-    unimplemented!()
+struct Point {
+    x: usize,
+    y: usize,
 }
 
-fn level_2(input: &str) -> R {
-    unimplemented!()
+const WIDTH: usize = 7;
+
+const SHAPES: [&[Point]; 5] = [
+    &[
+        Point { x: 0, y: 0 },
+        Point { x: 0, y: 1 },
+        Point { x: 0, y: 2 },
+        Point { x: 0, y: 3 },
+    ],
+    &[
+        Point { x: 1, y: 0 },
+        Point { x: 0, y: 1 },
+        Point { x: 1, y: 1 },
+        Point { x: 2, y: 1 },
+        Point { x: 1, y: 2 },
+    ],
+    &[
+        Point { x: 0, y: 0 },
+        Point { x: 0, y: 1 },
+        Point { x: 0, y: 2 },
+        Point { x: 1, y: 2 },
+        Point { x: 2, y: 2 },
+    ],
+    &[
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 0 },
+        Point { x: 2, y: 0 },
+        Point { x: 3, y: 0 },
+    ],
+    &[
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 0 },
+        Point { x: 0, y: 1 },
+        Point { x: 1, y: 1 },
+    ],
+];
+
+#[derive(Debug)]
+enum Jet {
+    Left,
+    Right,
+}
+
+impl From<char> for Jet {
+    fn from(c: char) -> Self {
+        match c {
+            '>' => Self::Right,
+            '<' => Self::Left,
+            _ => panic!("invalid input, {}", c),
+        }
+    }
+}
+
+#[derive(Default)]
+struct State {
+    map: Vec<[bool; WIDTH + 1]>,
+    top: usize,
+}
+
+impl State {
+    fn is_valid(&mut self, coord: &Point, shape: &[Point]) -> bool {
+        shape.iter().all(|offset| {
+            let x = offset.x + coord.x;
+            let y = offset.y + coord.y;
+
+            while self.map.len() <= x {
+                self.map.push([false; WIDTH + 1]);
+            }
+
+            y < WIDTH && !self.map[x][y]
+        })
+    }
+
+    fn signature(&self) -> u128 {
+        let rows: Vec<u128> = self
+            .map
+            .iter()
+            .rev()
+            .take(16)
+            .flatten()
+            .map(|&x| x.into())
+            .collect();
+
+        let mut signature = 0;
+        for (i, &x) in rows.iter().enumerate() {
+            signature |= x << i;
+        }
+        signature
+    }
+}
+
+impl fmt::Debug for State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "")?;
+
+        for i in self.map.iter().rev() {
+            write!(f, "|")?;
+            for &y in i {
+                if y {
+                    write!(f, "@")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f, "|")?;
+        }
+        writeln!(f, "+-------+")?;
+        Ok(())
+    }
 }
 
 fn main() {
     let now = std::time::Instant::now();
-    // let input = fs::read_to_string("src/bin/day17/input.in").expect("file not found");
+    let input = fs::read_to_string("src/bin/day17/input.in").expect("file not found");
 
-    // println!("level_1: {}", level_1(&input));
-    // println!("level_2: {}", level_2(&input));
-    let mut best_per_visited = vec![0; u16::MAX as usize];
-    dbg!(best_per_visited.len());
+    let jet_pattern: Vec<Jet> = input.trim().chars().map(Jet::from).collect();
 
+    let jet_lenght = jet_pattern.len();
+
+    let mut status = State::default();
+
+    let mut current_shape = 0;
+    let mut current_jet = 0;
+    let mut delta = 0;
+
+    let mut seen = HashMap::new();
+
+    let lenght = 1_000_000_000_000;
+    while current_shape < lenght {
+        let mut current_coord = Point {
+            x: status.top + 3,
+            y: 2,
+        };
+
+        loop {
+            let j = &jet_pattern[current_jet % jet_lenght];
+
+            let next_coord = Point {
+                x: current_coord.x,
+                y: {
+                    match j {
+                        Jet::Left => current_coord.y.saturating_sub(1),
+                        Jet::Right => current_coord.y + 1,
+                    }
+                },
+            };
+
+            if status.is_valid(&next_coord, SHAPES[current_shape % 5]) {
+                current_coord = next_coord;
+            }
+            current_jet += 1;
+
+            let next_coord = Point {
+                x: current_coord.x.saturating_sub(1),
+                y: current_coord.y,
+            };
+
+            if current_coord.x == 0 || !status.is_valid(&next_coord, SHAPES[current_shape % 5]) {
+                break;
+            }
+
+            current_coord = next_coord;
+        }
+
+        SHAPES[current_shape % 5].iter().for_each(|offset| {
+            let x = offset.x + current_coord.x;
+            let y = offset.y + current_coord.y;
+
+            while status.map.len() <= x {
+                status.map.push([false; WIDTH + 1]);
+            }
+
+            status.top = status.top.max(x + 1);
+            status.map[x][y] = true;
+        });
+
+        if let Some(value) = seen.insert(
+            (
+                current_jet % jet_lenght,
+                current_shape % 5,
+                status.signature(),
+            ),
+            (current_shape, status.top),
+        ) {
+            if current_shape > 2022 {
+                let (oldt, oldy) = value;
+                let dy = current_shape - oldt;
+                let dt = status.top - oldy;
+                let amt = (lenght - current_shape).div_euclid(dy);
+
+                delta += amt * dt;
+                current_shape += amt * dy;
+            }
+        }
+
+        current_shape += 1;
+        if current_shape == 2022 {
+            println!("part_1: {:?}", status.top);
+        }
+    }
+
+    println!("part_2: {:?}", status.top + delta);
     println!("time: {:?}", now.elapsed());
 }
--- src/bin/day18/main.rs
+++ src/bin/day18/main.rs
@@ -1,18 +1,104 @@
-use std::fs;
+use std::{fs, collections::{HashSet, VecDeque}, hash::Hash};
 
-type R = String;
+fn level_1(input: &str) -> u32 {
+    let rows:HashSet<(usize,usize,usize)> = input.lines().map(|s| {
+        let mut coords = s.split(",");
+        let (Some(x),Some(y),Some(z)) = (coords.next(),coords.next(),coords.next()) else{
+            panic!("format invalid");
+        };
+        (x.parse().unwrap(),y.parse().unwrap(),z.parse().unwrap())
+    }).collect();
 
-fn level_1(input: &str) -> R {
-    unimplemented!()
+    let total_sides = 6 * rows.len();
+    let mut connections = 0;
+
+
+    for row in &rows{
+        for (dx,dy,dz,fwd) in [(1,0,0,true),(1,0,0,false),(0,1,0,true),(0,1,0,false),(0,0,1,true),(0,0,1,false)]{
+            let coord = if fwd{
+                (row.0+dx,row.1+dy,row.2+dz)
+            }else{
+                (row.0-dx,row.1-dy,row.2-dz)
+            };
+            if rows.contains(&coord){
+                connections+=1;
+            }
+        }
+    }
+
+    total_sides as u32 -connections
 }
 
-fn level_2(input: &str) -> R {
-    unimplemented!()
+fn level_2(input: &str) -> u32 {
+    // 0 is empty
+    // 1 is lava
+    // 2 is outside air
+    let mut map = vec![vec![vec![0_u8;22];22];22];
+
+    input.lines().for_each(|s| {
+        let mut coords = s.split(",");
+        let (Some(x),Some(y),Some(z)) = (coords.next(),coords.next(),coords.next()) else{
+            panic!("format invalid");
+        };
+        map[x.parse::<usize>().unwrap()+1][y.parse::<usize>().unwrap()+1][z.parse::<usize>().unwrap()+1] = 1
+    });
+
+    let rows:HashSet<(usize,usize,usize)> = input.lines().map(|s| {
+        let mut coords = s.split(",");
+        let (Some(x),Some(y),Some(z)) = (coords.next(),coords.next(),coords.next()) else{
+            panic!("format invalid");
+        };
+        (x.parse::<usize>().unwrap()+1,y.parse::<usize>().unwrap()+1,z.parse::<usize>().unwrap()+1)
+    }).collect();
+
+    for (x,y,z) in &rows{
+        map[*x][*y][*z] = 1
+    }
+
+    let mut queue = VecDeque::new();
+    let mut seen = HashSet::new();
+
+    queue.push_back((0,0,0));
+
+    while let Some(value) = queue.pop_front(){
+        if seen.insert(value) && map[value.0][value.1][value.2] == 0 {
+            map[value.0][value.1][value.2] = 2;
+            for (dx,dy,dz,fwd) in [(1_i32,0_i32,0_i32,true),(1_i32,0_i32,0_i32,false),(0_i32,1_i32,0_i32,true),(0_i32,1_i32,0_i32,false),(0_i32,0_i32,1_i32,true),(0_i32,0_i32,1_i32,false)]{
+                let coord = if fwd{
+                    (value.0 as i32 +dx,value.1 as i32+dy,value.2 as i32+dz)
+                }else{
+                    (value.0 as i32-dx,value.1 as i32-dy,value.2 as i32-dz)
+                };
+
+                if coord.0 >= 0 && coord.1 >= 0 && coord.2 >= 0 && coord.0 < 22 && coord.1 < 22 && coord.2 < 22 {
+                    queue.push_back((coord.0 as usize,coord.1 as usize,coord.2 as usize))
+                }
+            }
+        }
+    }
+
+    let mut connections = 0;
+
+
+    for row in &rows{
+        for (dx,dy,dz,fwd) in [(1,0,0,true),(1,0,0,false),(0,1,0,true),(0,1,0,false),(0,0,1,true),(0,0,1,false)]{
+            let coord = if fwd{
+                (row.0+dx,row.1+dy,row.2+dz)
+            }else{
+                (row.0-dx,row.1-dy,row.2-dz)
+            };
+            if map[coord.0][coord.1][coord.2] == 2 {
+                connections+=1;
+            }
+        }
+    }
+
+    connections
 }
 
 fn main() {
     let now = std::time::Instant::now();
-    let input = fs::read_to_string("src/bin/dayXX/input.in").expect("file not found");
+    let input = fs::read_to_string("src/bin/day18/input.in").expect("file not found");
 
     println!("level_1: {}", level_1(&input));
     println!("level_2: {}", level_2(&input));
